6e5472cba9eebca1b944dc538c891ef234f110d3
João Vitor Waldrich <jv.waldrich0@protonmail.com>
2021-02-14 15:43:52 -0300

    Remove Everything

--- src/apps/forum/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import question
-
-class forum_home(ListView):
-    model = question
-    template_name = 'forum.html'
--- src/apps/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ExtendsUser
-
-# Register your models here.
-admin.site.register(ExtendsUser)
