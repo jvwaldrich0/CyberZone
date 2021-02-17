07a6c9c438a3ef45b85cf0934bba178c89b1a124
João Vitor Waldrich <jv.waldrich0@protonmail.com>
2021-02-17 08:47:24 -0300

    V0.2

--- src/apps/users/form.py
+++ src/apps/users/form.py
@@ -6,11 +6,11 @@ from apps.users.models import ExtendsUser
 
 
 class UserEditForm(UserChangeForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color:green;'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color:green;'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color:green;'}))
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color:green;'}))
-    password = forms.BooleanField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password', 'style': 'background-color:black; color:grey;border-color:green;'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
+    password = forms.BooleanField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
     
         
     class Meta:
@@ -20,9 +20,9 @@ class UserEditForm(UserChangeForm):
 
 
 class UserPasswordForm(PasswordChangeForm):
-    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password', 'style': 'background-color:black; color:grey;border-color:green;'}))
-    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password', 'style': 'background-color:black; color:grey;border-color:green;'}))
-    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password', 'style': 'background-color:black; color:grey;border-color:green;'}))
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password', 'style': 'background-color:black; color:grey;border-color: #242847;'}))
     
     class Meta:
         model = User
